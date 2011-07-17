7d3ffdf5f256800b1bd9933d72b5a9e62f4806e6
akira345 <dosmania@dosmania.dip.jp>
2011-07-17 18:02:28 +0900

    DBをmysqlにしてみた

--- railstest/config/initializers/secret_token.rb
+++ railstest/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Railstest::Application.config.secret_token = '38ad63dc516c0a62d0868041ba9b9362a8a3149fd58250f481559e6df327ca5400e6044e28ce5139e4640b7f2e34d4f50880bf0a11935eb83e1d8baa412110e2'
+Railstest::Application.config.secret_token = 'c23f94306f04e9a3f7464fdc08b0e531b715353c9b87dd668768444273b2fb3590cd2f66cd01214cde163de5a84db84eb1c40f09de33dff07a308ccc73896eb1'
