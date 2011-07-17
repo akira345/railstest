e2474f210b07639871cb80c07f37b88aecb57cde
akira345 <dosmania@dosmania.dip.jp>
2011-07-17 17:55:19 +0900

    first commit

---
+++ railstest/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railstest::Application.config.secret_token = '38ad63dc516c0a62d0868041ba9b9362a8a3149fd58250f481559e6df327ca5400e6044e28ce5139e4640b7f2e34d4f50880bf0a11935eb83e1d8baa412110e2'
