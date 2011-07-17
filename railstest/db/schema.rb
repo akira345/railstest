f2c6b31fc8f7903e0a037e0e01acc87b895435b4
akira345 <dosmania@dosmania.dip.jp>
2011-07-18 02:49:41 +0900

    とりあえず完成

--- railstest/db/schema.rb
+++ railstest/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110717163143) do
+ActiveRecord::Schema.define(:version => 20110717170018) do
 
   create_table "tweets", :force => true do |t|
     t.string   "tweet"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
