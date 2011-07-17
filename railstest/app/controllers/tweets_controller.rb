720caf94eb89fbc7460c8993b9fac92705ee0a0c
akira345 <dosmania@dosmania.dip.jp>
2011-07-18 01:51:23 +0900

    deviseによるユーザ認証まで

--- railstest/app/controllers/tweets_controller.rb
+++ railstest/app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_filter :authenticate_user!
   # GET /tweets
   # GET /tweets.xml
   def index
--- railstest/config/routes.rb
+++ railstest/config/routes.rb
@@ -1,4 +1,6 @@
 Railstest::Application.routes.draw do
+  devise_for :users
+
   resources :tweets
 
   # The priority is based upon order of creation:
