b43e4f5cd93274382731e65bea9a85d14e70c575
akira345 <dosmania@dosmania.dip.jp>
2011-07-25 21:45:43 +0900

    パスワード変更メールでエラーが出るバグを修正

--- railstest/config/environments/development.rb
+++ railstest/config/environments/development.rb
@@ -22,5 +22,17 @@ Railstest::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.localhost.localdomain",
+    :port => 25,
+    :domain => "localdomain",
+#    :user_name => "",
+#    :password => "",
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
 
